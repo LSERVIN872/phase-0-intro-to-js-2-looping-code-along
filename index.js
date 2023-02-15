01294e101c56852f796092bf10d2b701804ae356
luis <servinluis21@yahoo.com>
2023-02-14 19:08:45 -0500

    complete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+    let strings = []
+    for (let i = 0; i < names.length; i++) {
+    strings.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return strings
+}
+function countDown(){
+    let countdown = 10;
+
+    while (countdown >= 0) {
+      console.log(--countdown)
+    }}
+
